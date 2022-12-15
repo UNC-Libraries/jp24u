308137ed2bd4acb8ba33c84eba7da90e342de8ae
krwong <krwong@email.unc.edu>
2022-12-15 15:20:31 -0500

    add TemporaryImageService and tests

---
+++ src/main/java/colorscanner/services/TemporaryImageService.java
@@ -0,0 +1,41 @@
+package colorscanner.services;
+
+import org.slf4j.Logger;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Service for converting a CMYK source image to a temporary image file before kakadu jp2 compression
+ * @author krwong
+ */
+public class TemporaryImageService {
+    private static final Logger log = getLogger(TemporaryImageService.class);
+
+    //TODO: we will need to test different CMYK conversion options
+    //It seems like only using Color Space creates a more color accurate temporary image.
+    //Using Color Space and ICC Profile or just the ICC Profile create a temporary image with slightly different colors.
+    public void convertImage(String fileName) throws Exception {
+        String convert = "convert";
+        //String profile = "-profile";
+        //String profileOptions = "src/main/resources/AdobeRGB1998.icc";
+        String colorSpace = "-colorspace";
+        String colorSpaceOptions = "srgb";
+        String temporaryFile = fileName + ".jpg";
+
+        List<String> command = Arrays.asList(convert, fileName, colorSpace, colorSpaceOptions,
+                temporaryFile);
+
+        try {
+            ProcessBuilder builder = new ProcessBuilder(command);
+            builder.redirectErrorStream(true);
+            Process process = builder.start();
+            String cmdOutput = new String(process.getInputStream().readAllBytes());
+            log.info(cmdOutput);
+        } catch (Exception e) {
+            throw new Exception(fileName + " failed to generate jpg file.", e);
+        }
+    }
+}
---
+++ src/test/java/colorscanner/services/TemporaryImageServiceTest.java
@@ -0,0 +1,53 @@
+package colorscanner.services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertTrue;
+
+public class TemporaryImageServiceTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    private TemporaryImageService service;
+
+    @Before
+    public void setup() throws Exception {
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        service = new TemporaryImageService();
+    }
+
+    @Test
+    public void testConvertImageWithIccProfile() throws Exception {
+        String testFile1 = "src/test/resources/OP20459_1_TremorsKelleyandtheCowboys.tif";
+        service.convertImage(testFile1);
+
+        assertTrue(Files.exists(Paths.get("src/test/resources/OP20459_1_TremorsKelleyandtheCowboys.tif.jpg")));
+    }
+
+    @Test
+    public void testConvertImageWithoutIccProfile() throws Exception {
+        String testFile = "src/test/resources/Surgery.tif";
+        service.convertImage(testFile);
+
+        assertTrue(Files.exists(Paths.get("src/test/resources/Surgery.tif.jpg")));
+    }
+
+    @Test
+    public void testConvertImageWithMultipleTemporaryImages() throws Exception {
+        String testFile = "src/test/resources/SAAACAM-HopeHouse_transparency_with-title_merged.tif";
+        service.convertImage(testFile);
+
+        assertTrue(Files.exists(Paths.get("src/test/resources/SAAACAM-HopeHouse_transparency_with" +
+                "-title_merged.tif-0.jpg")));
+        assertTrue(Files.exists(Paths.get("src/test/resources/SAAACAM-HopeHouse_transparency_with" +
+                "-title_merged.tif-1.jpg")));
+    }
+
+
+}
