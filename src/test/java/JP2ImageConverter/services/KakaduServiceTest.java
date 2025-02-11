ff61afe8f0f44f9cc45387101f842f2aa925abb7
krwong <krwong@email.unc.edu>
2025-02-11 10:03:58 -0500

    add type param to convertColorspaces, add test

--- src/test/java/JP2ImageConverter/services/KakaduServiceTest.java
+++ src/test/java/JP2ImageConverter/services/KakaduServiceTest.java
@@ -391,6 +391,36 @@ public class KakaduServiceTest {
         assertContains("2832x4256", output);
     }
 
+    @Test
+    public void testKduCompressTiffWithPaletteTypeAndColorSpace() throws Exception {
+        String mockedTif = String.valueOf(new File( tmpFolder + "/mockedImage.tif"));
+        Map<String, String> imageMetadata = Map.of(ColorFieldsService.COLOR_SPACE, "RGB Palette");
+        ColorFieldsService colorFieldsService = mock(ColorFieldsService.class);
+        when(colorFieldsService.extractMetadataFields(anyString())).thenReturn(imageMetadata);
+        when(colorFieldsService.identifyType(anyString())).thenReturn("Palette");
+        ImagePreproccessingService imagePreproccessingService = mock(ImagePreproccessingService.class);
+        when(imagePreproccessingService.convertToTiff(anyString(), anyString())).thenReturn(mockedTif);
+        when(imagePreproccessingService.convertColorSpaces(anyString(), anyString())).thenReturn(mockedTif);
+
+        try (MockedStatic<CommandUtility> mockedStatic = Mockito.mockStatic(CommandUtility.class)) {
+            File mockedJp2 = new File(tmpFolder + "/mockedImage.jp2");
+            mockedStatic.when(() -> CommandUtility.executeCommand(anyList())).thenReturn(String.valueOf(mockedJp2));
+
+            KakaduService service = new KakaduService();
+            service.setColorFieldsService(colorFieldsService);
+            service.setImagePreproccessingService(imagePreproccessingService);
+            service.kduCompress(mockedTif, tmpFolder.resolve("mockedImage"), "");
+
+            mockedStatic.verify(() -> CommandUtility.executeCommand(
+                    new ArrayList<>(Arrays.asList("kdu_compress", "-i", mockedTif, "-o", mockedJp2.toString(),
+                            "Clevels=6", "Clayers=6", "Cprecincts={256,256},{256,256},{128,128}", "" +
+                            "Stiles={512,512}", "Corder=RPCL", "ORGgen_plt=yes", "ORGtparts=R", "Cblk={64,64}",
+                            "Cuse_sop=yes", "Cuse_eph=yes", "-flush_period", "1024", "-rate", "3", "-no_weights"))));
+            verify(imagePreproccessingService, times(1)).convertToTiff(mockedTif, "tif");
+            verify(imagePreproccessingService, times(1)).convertColorSpaces("RGB Palette", "Palette", mockedTif);
+        }
+    }
+
     private void assertContains(String expected, String actual) {
         assertTrue(actual.contains(expected), "Expected string '" + expected + "' not found: " + actual);
     }
